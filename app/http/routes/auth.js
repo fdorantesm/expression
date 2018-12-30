fa55b0560348435ab2f5e059b448824bb8b1a6e5
Fernando Dorantes <fdorantesm@gmail.com>
2018-12-30 12:11:17 -0600

    Removed login from social auth
     Changes to be committed:
    	modified:   app/http/routes/auth.js

--- app/http/routes/auth.js
+++ app/http/routes/auth.js
@@ -5,8 +5,8 @@ import middleware from 'middleware/auth'
 const router = Router()
 
 router.post('/login', AuthController.login)
-router.post('/facebook/login', AuthController.facebook)
-router.post('/google/login', AuthController.google)
+router.post('/facebook', AuthController.facebook)
+router.post('/google', AuthController.google)
 router.post('/register', AuthController.register)
 
 router.get('/whoami', middleware.authenticated, AuthController.whoami)
