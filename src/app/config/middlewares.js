d7f367d5e962cb3ce1cca382d88518e8677d3d2f
Fernando Dorantes <fdorantes.m@gmail.com>
2019-02-16 15:46:59 -0600

    Hide stack in production

--- src/app/config/middlewares.js
+++ src/app/config/middlewares.js
@@ -51,9 +51,8 @@ export default (app) => {
 
 	app.use((err, req, res, next) => {
 		res.locals.message = err.message
-		res.locals.error = req.app.get('env').ENV === 'local' ? err : {}
 		res.status(err.status || 500)
-		res.render('error')
+		res.render('error', {env: req.app.get('env')})
 	})
 
 	app.use(compression())
