f9c40904f9a7828844fbd1451dd758625c4d223e
Fernando Dorantes <fdorantes.m@gmail.com>
2019-02-16 14:10:23 -0600

    ES6 building
    Instaled babel dependencies to avoid babel-node in production
    
     Changes to be committed:
    	new file:   babel.config.js
    	modified:   package-lock.json
    	modified:   package.json

---
+++ babel.config.js
@@ -0,0 +1,48 @@
+module.exports = function (api) {
+
+	api.cache(true);
+
+	const presets = [
+		"@babel/preset-env"
+	];
+	
+	const plugins = [
+		[
+			"module-resolver", {
+				"root": [
+					"./src/app"
+				],
+				"alias": {
+					"bin": "./src/bin",
+					"config": "./src/app/config",
+					"model": "./src/app/models",
+					"controller": "./src/app/http/controllers",
+					"middleware": "./src/app/http/middlewares",
+					"route": "./src/app/http/routes",
+					"locale": "./src/app/locales",
+					"log": "./src/app/logs",
+					"library": "./src/app/libraries",
+					"helper": "./src/app/helpers",
+					"view": "./src/app/views",
+					"app": "./src/app/index",
+					"events": "./src/app/events",
+					"loader": "./src/app/loader",
+					"router": "./src/app/libraries/router",
+					"routes": "./src/app/http/router",
+					"www": "./src/bin/www",
+					"server": "./src/bin/server"
+				}
+			}
+		],
+			
+		[
+			"@babel/plugin-transform-runtime"
+		]
+
+	];
+
+	return {
+		presets,
+		plugins
+	};
+}
